7ebe4be77138d33f02d48aa94ed6fe8d0a3767da
Chris Grice <chris@chrisgrice.com>
2013-01-25 12:51:15 +0000

    Parsed response for imdb title query

--- imdb/api.py
+++ imdb/api.py
@@ -18,7 +18,11 @@ def actor(name, **kwargs):
 def film(name, **kwargs):
 
     response = _work(name)
+
     film = Film()
+    film.imdb_id = response['id']
+    film.name = response['title']
+    film.short_desc = response['description']
     return film
 
 
@@ -40,6 +44,16 @@ def _person(name):
 def _work(name):
     response = _request("http://www.imdb.com/xml/find?json=1&nr=1&tt=on&q=", name)
 
+    if 'title_popular' in response:
+        work = response['title_popular'][0]
+    elif 'title_exact' in response:
+        work = response['title_exact'][0]
+    elif 'title_approx' in response:
+        work = response['title_approx'][0]
+    else:
+        work = False
+
+    return work
 
 
 def _request(base_url, name):
